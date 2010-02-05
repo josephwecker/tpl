63ee3719e373d2be974d4aafec3316798d0ee147
Troy D. Hanson <tdh@tkhanson.net>
2010-02-05 02:00:33 -0800

    s/2009/2010

--- src/tpl.h
+++ src/tpl.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2005-2009, Troy D. Hanson     http://tpl.sourceforge.net
+Copyright (c) 2005-2010, Troy D. Hanson     http://tpl.sourceforge.net
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
